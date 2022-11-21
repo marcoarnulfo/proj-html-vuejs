7440935223fa8e048b4d780333b47b0755440078
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-11-21 12:15:39 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import {store} from './store.js';
+export default {
+  name: 'App',
+  data(){
+    return{
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+</style>
